d57dc7c16665334cac899f1f45f1c7dac037cc6c
eclipse_user <eclipse_user@qq.com>
2020-02-02 10:47:38 +0800

    My First commit in Ecipse

---
+++ src/main/java/wwj/qk/maven/Apple.java
@@ -0,0 +1,7 @@
+package wwj.qk.maven;
+
+public class Apple {
+    public static void main(String[] args) {
+		System.out.println("I am an apple");
+	}
+}
---
+++ src/test/java/wwj/qk/maven/GoodTest.java
@@ -0,0 +1,7 @@
+package wwj.qk.maven;
+
+public class GoodTest {
+    public static void main(String[] args) {
+		System.out.println("I am a good Test");
+	}
+}
