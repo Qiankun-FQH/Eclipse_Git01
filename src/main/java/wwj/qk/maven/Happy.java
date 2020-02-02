20748557482ad986bb46ffde2f177a16e2673d59
eclipse_user <eclipse_user@qq.com>
2020-02-02 11:18:23 +0800

    GitStudy01 is so happy

--- src/main/java/wwj/qk/maven/Happy.java
+++ src/main/java/wwj/qk/maven/Happy.java
@@ -2,6 +2,6 @@ package wwj.qk.maven;
 
 public class Happy {
     public static void main(String[] args) {
-		System.out.println("i am so happy");
+		System.out.println("GitStudy01 am so happy");
 	}
 }
