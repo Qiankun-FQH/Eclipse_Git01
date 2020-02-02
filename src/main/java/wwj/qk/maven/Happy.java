7c5468a5ac961b7f77e8835a76cb2194aa01792c
qk <qk@qq.com>
2020-02-02 11:30:47 +0800

    All happy

--- src/main/java/wwj/qk/maven/Happy.java
+++ src/main/java/wwj/qk/maven/Happy.java
@@ -5,6 +5,6 @@ public class Happy {
 		System.out.println("AAAAAAAAAAAAEclipse_Git01_Copy2 am so happy");
 		System.out.println("AAAA");
 		System.out.println("GitStudy01 am so happy");
-		
+		System.out.println("¶¼¿ªÐÄ");
 	}
 }
